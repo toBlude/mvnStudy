37fe8ea2bfbca937c4d051ca3ee5231c2b245395
bludee <294021544@qq.com>
2016-02-18 17:06:04 +0800

    修改测试

--- src/main/java/com/bludee/App.java
+++ src/main/java/com/bludee/App.java
@@ -9,10 +9,7 @@ public class App
     public static void main( String[] args ) throws Exception
     {
         int port=9000;
-        System.out.println("server started");
+        System.out.println("server ok");
         new DiscardServer(port).run();
-    }
-    public void testFun(){
-        System.out.println("committest");
     }
 }
